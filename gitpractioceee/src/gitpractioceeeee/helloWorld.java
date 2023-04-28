afdbe2a95c3cef20a430ad59539c84621bb06ddf
HP <HP@LAPTOP-DBLBVRKN>
2023-04-28 12:48:41 +0530

    maven and testng commit

--- gitpractioceee/src/gitpractioceeeee/helloWorld.java
+++ gitpractioceee/src/gitpractioceeeee/helloWorld.java
@@ -1,8 +1,12 @@
 package gitpractioceeeee;
 
+import org.testng.annotations.Test;
+
 public class helloWorld {
 
-	public static void main(String[] args) {
+	@Test
+	public void method()
+	{
 		// TODO Auto-generated method stub
 
 		System.out.println("Hello world");
