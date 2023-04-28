94da1a4e5e3421078ac0183275ddbd4380434d52
HP <HP@LAPTOP-DBLBVRKN>
2023-04-28 11:18:42 +0530

    first commit

--- gitpractioceee/src/gitpractioceeeee/helloWorld.java
+++ gitpractioceee/src/gitpractioceeeee/helloWorld.java
@@ -1,12 +1,11 @@
-package Github;
+package gitpractioceeeee;
 
 public class helloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
 
-		System.out.println("Hi");
+		System.out.println("Hello world");
 	}
 
 }
